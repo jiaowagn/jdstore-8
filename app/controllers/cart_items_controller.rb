f9bb3d2871436b6859b78d367b246b04175335c6
jiaowagn <wangjiaohere@gmail.com>
2017-09-25 21:35:18 +0800

    delete an item from cart

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,9 @@
+class CartItemsController < ApplicationController
+  def destroy
+    @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
+    flash[:warning] = "成功将 #{@product.title} 从购物车删除"
+    redirect_to :back
+  end
+end
